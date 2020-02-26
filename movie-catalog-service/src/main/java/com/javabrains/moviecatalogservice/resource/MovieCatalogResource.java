665b4af9abf60a666bc100790f46bbcf2876e6ee
Jitender Kumar <jitender.kumar@irissoftware.com>
2020-02-26 11:20:23 +0530

    Added dependency and code for reactive programming - webclient

--- movie-catalog-service/src/main/java/com/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 import com.javabrains.moviecatalogservice.model.CatalogItem;
 import com.javabrains.moviecatalogservice.model.Movie;
@@ -23,14 +25,23 @@ public class MovieCatalogResource {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Autowired
+	WebClient.Builder webClientBuilder;
+	
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId) {
+		
+		//To Build web client
+		//WebClient.Builder builder = WebClient.builder();
 		
 		List<Rating> ratings = Arrays.asList(new Rating("1234", 4),
 				new Rating("5678",3));
 		
 		return ratings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+			//Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+			
+			//Using weclient builder
+			Movie movie = webClientBuilder.build().get().uri("http://localhost:8081/movies/" + rating.getMovieId()).retrieve().bodyToMono(Movie.class).block();
 			return new CatalogItem(movie.getName(), "Test", rating.getRating());
 		}).collect(Collectors.toList());
 		
