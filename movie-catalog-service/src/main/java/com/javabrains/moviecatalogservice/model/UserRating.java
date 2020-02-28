428a353dd86e9bcbda8fb73bef3a5be76630e5c4
Jitender Kumar <jitender.kumar@irissoftware.com>
2020-02-28 13:40:08 +0530

    Added Eureka server project

--- movie-catalog-service/src/main/java/com/javabrains/moviecatalogservice/model/UserRating.java
+++ movie-catalog-service/src/main/java/com/javabrains/moviecatalogservice/model/UserRating.java
@@ -3,6 +3,13 @@ package com.javabrains.moviecatalogservice.model;
 import java.util.List;
 
 public class UserRating {
+	
+	
+	public UserRating(List<Rating> userRating) {
+		super();
+		this.userRating = userRating;
+	}
+
 	private List<Rating> userRating;
 
 	public List<Rating> getUserRating() {
