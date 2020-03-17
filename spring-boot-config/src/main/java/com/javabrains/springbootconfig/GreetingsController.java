134bbab897fe0aaf2ba271d4971bbaf716e4da33
Jitender Kumar <jitender.kumar@irissoftware.com>
2020-03-17 14:34:40 +0530

    Using Environment properties

--- spring-boot-config/src/main/java/com/javabrains/springbootconfig/GreetingsController.java
+++ spring-boot-config/src/main/java/com/javabrains/springbootconfig/GreetingsController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,9 +31,19 @@ public class GreetingsController {
 	@Autowired
 	private DbSettings dbSettings;
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/{userName}")
 	public String greeting(@PathVariable String userName) {
 		//return greetingMessage + " " + staticMessage + listValues + dbValues;
 		return dbSettings.getConnection() + dbSettings.getHost();
 	}
+	
+	@GetMapping("/envdetails")
+	public String envDetails() {
+		//return greetingMessage + " " + staticMessage + listValues + dbValues;
+		return env.toString();
+	}
+
 }
